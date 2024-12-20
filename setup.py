179ce31c44b6546603bf7c48417af72f1a72bd65
Aditya_Thakare <adityathakare72@gmail.com>
2024-12-20 13:06:14 +0530

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='model for predicting if loan will be approved or not',
+    author='aditya',
+    license='',
+)
